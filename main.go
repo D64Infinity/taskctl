f708642135e23aaf600a0a7c297e00630c823274
D64Infinity <makartcev.ss@students.dvfu.ru>
2026-05-03 14:45:00 +1000

    Connection to database

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	fmt.Println("go.mod init")
+	connStr := "postgres://postgres:12345678@localhost:5432/taskctl"
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+	}
+	defer conn.Close(context.Background())
+
+	fmt.Println("Connected to database")
 }
