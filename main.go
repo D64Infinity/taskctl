29a7602acd64548ebe3cc3daa235060b86e8d45e
D64Infinity <makartcev.ss@students.dvfu.ru>
2026-05-03 14:55:09 +1000

    First task added

--- main.go
+++ main.go
@@ -17,5 +17,18 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	fmt.Println("Connected to database")
+	taskDescription := "Practice Golang"
+
+	var taskId int
+	err = conn.QueryRow(
+		context.Background(),
+		"INSERT INTO tasks (description) VALUES ($1) RETURNING id",
+		taskDescription,
+	).Scan(&taskId)
+
+	if err != nil {
+		log.Fatal("Failed to insert the task: ", err)
+	}
+
+	fmt.Printf("Task added with ID: %d\n", taskId)
 }
